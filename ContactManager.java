da04bdb7d3b597fd0f77a2dd573d03d29719b25a
duck5 <duckhoim@kyonggi.ac.kr>
2018-04-30 02:07:07 +0900

    Update ContactManager.java

--- ContactManager.java
+++ ContactManager.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
 class Contact
 {
 	private String name;
@@ -11,14 +13,6 @@ class Contact
 		this.name = name;
 		this.age = age;
 		this.phoneNum = phoneNum;
-	}
-	public Contact(String name, String phoneNum)
-	{
-		this(name, 0, phoneNum);
-	}
-	public Contact(String phoneNum)
-	{
-		this("unknown", 0, phoneNum);
 	}
 	public String getName() { return name; }
 	public int getAge() { return age; }
@@ -43,6 +37,7 @@ class Contact
 public class ContactManager {
 	private ArrayList<Contact> phoneBook;
 	private Scanner scanner;
+	boolean sortMode = false;
 	public ContactManager() 
 	{
 		phoneBook = new ArrayList<Contact>();
@@ -51,50 +46,94 @@ public class ContactManager {
 	
 	public void showMenu() {
 
-			System.out.println("############ 연락처 관리 ############");
-			System.out.println("1. 연락처 출력");
-			System.out.println("2. 연락처 등록");
-			System.out.println("3. 연락처 삭제");
-			System.out.println("4. 연락처 수정");
-			System.out.println("5. 연락처 검색");
-			System.out.println("6. 출력 설정");
-			System.out.println("7. 끝내기");
-			System.out.print("선택 : ");
-
+		System.out.println("############ 연락처 관리 ############");
+		System.out.println("1. 연락처 출력");
+		System.out.println("2. 연락처 등록");
+		System.out.println("3. 연락처 삭제");
+		System.out.println("4. 연락처 수정");
+		System.out.println("5. 연락처 검색");
+		System.out.println("6. 추가기능");
+		System.out.println("7. 끝내기");
+			
+	}
+	
+	public void showAddiMenu() {
+		System.out.println("############ 추가 기능 ############");
+		if(sortMode)
+			System.out.println("1. 가나다순 정렬 해제");
+		else
+			System.out.println("1. 가나다순 정렬 설정");
+		System.out.println("2. 즐겨찾기");
 	}
 	
 	public void showList() {
 		int i = 1;
-		for(Contact data:phoneBook) {
-			System.out.println(i++ +"."+data);
+		if(sortMode) {	
+			TreeMap<String, Contact> dataList = new TreeMap<String, Contact>();
+			for(Contact data : phoneBook)
+			{
+				dataList.put(data.getName()+(i++), data);
+			}
+			i = 1;
+			for(String name : dataList.keySet())
+			{
+				System.out.println(i++ +"."+ dataList.get(name));
+			}
+		} else {
+			for(Contact data:phoneBook) {
+				System.out.println(i++ +"."+data);
+			}
 		}
 	}
 	
 	public void regist() {
 		
-		String name, phoneNum = null;
-		int age;
+		String name = null, phoneNum = null;
+		int age = 0;
 		boolean isDuplicate = false;
+		boolean rightInput = false;
 		
 		System.out.print("이름: ");
 		name = scanner.nextLine();
-		System.out.print("나이: ");
-		age = scanner.nextInt();
-		scanner.nextLine(); // clear \n
+		do
+		{
+			try {
+				System.out.print("나이: ");
+				age = scanner.nextInt();
+				if(age<0)
+					throw new Exception("나이는 음수가 될 수 없습니다.");
+				rightInput = true;
+			} catch (InputMismatchException ie) {
+				System.out.println("나이는 정수만 입력해 주세요.");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			} finally {
+				if(scanner.hasNextLine())
+					scanner.nextLine(); // clear buffer
+			}
+		} while(!rightInput);
 		
 		do {
-			System.out.print("전화번호: ");
 			try {
+				System.out.print("전화번호: ");
 				phoneNum = scanner.nextLine();
+				String checkRegex = "^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$";
+				if(Pattern.matches(checkRegex, phoneNum) == false)  
+					throw new Exception("비정상입력");
+				rightInput = true;
 				for(Contact data : phoneBook)
 					if(data.getPhoneNum().equals(phoneNum))
 						throw new Exception("전화번호가 다른 연락처와 중복되었습니다. ");
 				isDuplicate = false;
 			} catch(Exception e) {
+				if(e.getMessage().equals("비정상입력"))
+					rightInput = false;
+				else
+					isDuplicate = true;
 				System.out.println(e.getMessage());
-				isDuplicate = true;
 			}
-		} while(isDuplicate);
+		} while(isDuplicate || !(rightInput));
+		
 		phoneBook.add(new Contact(name, age, phoneNum));
 	}
 
@@ -104,58 +143,58 @@ public class ContactManager {
 			System.out.print("삭제할 행번호: ");
 			try {
 				index = scanner.nextInt()-1;
-				scanner.nextLine(); // clear \n
 				if( index > phoneBook.size() || index < 0 )
 					throw new Exception("삭제할 행 번호가 없습니다.");
 			} catch(InputMismatchException ie) {
 				System.out.println("정수만 입력해주세요.");
 			} catch(Exception e) {
 				e.getMessage();
+			} finally {
+				if(scanner.hasNextLine())
+					scanner.nextLine();
 			}
 			System.out.println( "["+ phoneBook.get(index)+"]"+"가 삭제 되었습니다.");
 			phoneBook.remove(index);
 	}
 	
-	public void edit() {
-
-		boolean rightInput = false;
-		int index = 0;
-		String newNum = null;
-		
-		while(!rightInput)
-		{
-			System.out.print("수정할 행번호 : ");
-			index = scanner.nextInt() - 1;
-			scanner.nextLine(); // clear \n
-			
-			try {
-				System.out.println("수정할 번호를 입력하시오");
-				newNum = scanner.nextLine();
-				
-				rightInput = true;
-			}
-			catch(InputMismatchException e) {
-				System.out.println("번호를 다시 입력해주세요");
-			}
-		}
-		phoneBook.get(index).edit(newNum);
-		System.out.println("수정이 완료되었습니다.");
-	}
+	public void edit() { }
 
 	public void search() {
-		String keyword;
+		String keyword = null;
+		boolean rightInput = true;
+		boolean hasResult = false;
 		int i = 1;
-		System.out.print("검색어 : ");
-		keyword =  scanner.nextLine();
+		do {
+			
+			System.out.print("검색어 : ");
+			try {
+				keyword =  scanner.nextLine();
+				// 의미가 있을까?
+				if(!Pattern.matches("[0-9]+$", keyword))
+					throw new Exception("검색어는 숫자만 사용가능합니다.");
+				rightInput = true;
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
+				rightInput = false;
+			}
+			
+		} while(!rightInput);
 		
 		for(Contact data : phoneBook)
-		{ 
+		{
 			if(data.getPhoneNum().contains(keyword))
-				System.out.println(i+". "+data);
+			{				
+				hasResult = true;
+				System.out.println(i +". "+data);
+			}			
 			i++;
 		}
+		if(!hasResult) System.out.println("No results found");
 	}
 	
+	public void sortOnOff() {
+		sortMode = !sortMode;
+	}
 	public static void main(String[] args)
 	{
 		ContactManager CM = new ContactManager();
@@ -168,15 +207,18 @@ public class ContactManager {
 			CM.showMenu();
 			while(!rightInput)
 			{
+				System.out.print("선택 : ");
 				try {
 					act = CM.scanner.nextInt();
-					CM.scanner.nextLine(); // clear \n
-					if(act<1 || act>6) throw new InputMismatchException();
+					if(act<1 || act>7) throw new InputMismatchException();
 					rightInput = true;
 				} catch(InputMismatchException e) {
 					System.out.println("1~6 사이 정수만 입력해주세요.");
 				} catch(Exception e) {
 					e.printStackTrace();
+				} finally {
+					if(CM.scanner.hasNextLine())
+						CM.scanner.nextLine();
 				}
 			}
 			System.out.println();
@@ -185,7 +227,7 @@ public class ContactManager {
 					if(CM.phoneBook.size() != 0)
 						CM.showList();
 					else
-						System.out.println("출력할게 없다는 메시지");
+						System.out.println("연락처에 출력할 데이터가 없습니다.");
 					break;
 				case 2:
 					CM.regist();
@@ -194,15 +236,50 @@ public class ContactManager {
 					if(CM.phoneBook.size() != 0)
 						CM.delete();
 					else
-						System.out.println("삭제할게 없다는 메시지");
+						System.out.println("연락처에 삭제할 데이터가 없습니다.");
 					break;
 				case 4:
-					CM.edit();
+					if(CM.phoneBook.size() != 0)
+						CM.edit();
+					else
+						System.out.println("연락처에 수정할 데이터가 없습니다.");
 					break;
 				case 5:
-					CM.search();
+					if(CM.phoneBook.size() != 0)
+						CM.search();
+					else
+						System.out.println("연락처에 검색할 데이터가 없습니다.");
 					break;
 				case 6:
+					rightInput = false;
+					act = 0;
+					CM.showAddiMenu();
+					while(!rightInput)
+					{
+						System.out.print("선택 : ");
+						try {
+							act = CM.scanner.nextInt();
+							if(act<1 || act>2) throw new InputMismatchException();
+							rightInput = true;
+						} catch(InputMismatchException e) {
+							System.out.println("1~2 사이 정수만 입력해주세요.");
+						} catch(Exception e) {
+							e.printStackTrace();
+						} finally {
+							if(CM.scanner.hasNextLine())
+								CM.scanner.nextLine();
+						}
+					}
+					switch(act)
+					{
+						case 1:
+							CM.sortOnOff();	
+							break;
+						case 2:
+//							CM.favorite();
+					}
+					break;
+				case 7:
 					done = true;
 					break;
 			}
@@ -212,5 +289,4 @@ public class ContactManager {
 		}
 		CM.scanner.close();
 	}
-	
 }
